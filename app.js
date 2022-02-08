d2457175df04dee4ca99df928b32b122165e9243
bjfengweiping <fengweiping@jd.com>
2022-02-08 09:08:07 +0800

    增加中间件 ，res.cc方法替代res.send

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const userRouter = require('./router/user')
 // 创建 express 的服务器实例
 const app = express()
 
-// write your code here...
 // 导入 cors 中间件
 const cors = require('cors')
 // 将 cors 注册为全局中间件
@@ -13,6 +12,22 @@ app.use(cors())
 
 //配置解析 application/x-www-form-urlencoded 格式的表单数据的中间件
 app.use(express.urlencoded({ extended: false }))
+
+// 响应数据的中间件,优化res.send
+app.use((req,res,next) => {
+  res.cc =(error,status=1) => {
+    // status = 0 为成功； status = 1 为失败； 默认将 status 的值设置为 1，方便处理失败的情况
+    res.send({
+      // 状态
+      status,
+      // 状态描述，判断 err 是 错误对象 还是 字符串
+      error:error instanceof Error?error.message:error
+    })
+  };
+  
+  next()
+}
+)
 
 app.use('/api', userRouter)
 
--- router_handler/user.js
+++ router_handler/user.js
@@ -15,24 +15,23 @@ exports.regUser = (req, res) => {
 
     // 判断数据是否合法
     if (!userinfo.username || !userinfo.password) {
-        return res.send({ status: 1, message: '用户名或密码不能为空!!!' })
+        return res.cc('用户名或密码不能为空!!!');
     }
 
     // 对用户的密码,进行 bcrype 加密，返回值是加密之后的密码字符串
     userinfo.password = bcrypt.hashSync(userinfo.password, 10)
+    //INSERT INTO `jdw_db`.`ev_users` (`username`, `password`) VALUES ('lilong', '1234');
     const sql = 'insert into ev_users set ?'
     db.query(sql, { username: userinfo.username, password: userinfo.password }, function (err, results) {
       // 执行 SQL 语句失败
-      if (err) return res.send({ status: 1, message: err.message })
+      if (err) return res.cc(err)
       // SQL 语句执行成功，但影响行数不为 1
       if (results.affectedRows !== 1) {
-        return res.send({ status: 1, message: '注册用户失败，请稍后再试！' })
+        return res.cc('注册用户失败，请稍后再试！')
       }
       // 注册成功
-      res.send({ status: 0, message: '注册成功！' })
+      res.cc('注册成功！',0)
     })
-
-    
   }
   
   // 登录的处理函数
